2cb4d780c94f05fad995619b9f8cc48fbe250b7c
Samuel <samueledet717@gmail.com>
2026-06-16 14:10:24 +0100

    Fixed Email configuration

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -125,9 +125,16 @@ LOGIN_URL = 'login'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-EMAIL_BACKEND = (
-    'django.core.mail.backends.console.EmailBackend'
-)   
+
+# Email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'samueledet717@gmail.com'
+EMAIL_HOST_PASSWORD = 'kezf buaj fjst gqto'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
