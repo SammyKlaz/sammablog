3b6b356c474ed8a1f11aaae6108661eaede7fa53
Samuel <samueledet717@gmail.com>
2026-07-01 03:06:25 +0100

    Fixed smtp issues with emails

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -127,6 +127,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Email configuration
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
@@ -135,6 +136,8 @@ EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = 'samueledet717@gmail.com'
 EMAIL_HOST_PASSWORD = 'kezfbuajfjstgqto'
+
+DEFAULT_FROM_EMAIL = 'samueledet717@gmail.com'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
